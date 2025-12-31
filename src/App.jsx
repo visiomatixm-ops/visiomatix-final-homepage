2a2fe4737b68e9e238683e7c321821895d8d991c
muktanjali01 <muktanjalighangale@gmail.com>
2025-12-31 15:55:34 +0530

    Final home project created

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import "./App.css";
+
+// TEMP: testing one homepage
+import HomePage1 from "./components/home-page-1/HomePage1";
+
+function App() {
+  return (
+    <div className="app">
+      <HomePage1 />
+    </div>
+  );
+}
+
+export default App;
